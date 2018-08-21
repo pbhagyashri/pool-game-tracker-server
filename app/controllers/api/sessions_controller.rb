19b0a12f7b85e72242b6f2f1a5df3dd76ab9a68a
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-08-21 00:31:41 -0400

    in login action of sessions_controller change params[:email] to params[:user][:email]

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,19 @@
-class Api::GamesController < ApplicationController
+require 'auth'
+class Api::SessionsController < ApplicationController
 
     def login
 
         #take user's email and password from client and find user with the email, and if user is #found. authenticate with #password.  if user is authenticated, create a token for user and #decode it and send back user's details
+        
+        user = User.find_by(email: params[:user][:email])
 
-        user = User.find_by(email: params[:email])
-
-        if user && user.authenticate(params[:password])
+        if user && user.authenticate(params[:user][:password])
         
             token = Auth.create_token(user)
 
             returned_user = Auth.decode_token(token)
             
-            render json: {name: user.name, username: user.username, id: user.id, email: user.email, token: token}, status: 200
+            render json: {username: user.username, id: user.id, email: user.email, token: token}, status: 200
         else
             render json: {errors: "Email or Password is incorrect"}, status: 500
         end
