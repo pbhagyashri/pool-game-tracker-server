804cd59c4906005d39543481feb8ae106a46a7e1
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-08-20 21:36:59 -0400

    Add auth.rb file in libs directory. Add self.create_token and self.decode_token to auth.rb file

---
+++ lib/auth.rb
@@ -0,0 +1,21 @@
+require 'jwt'
+
+class Auth
+
+    #self.create_token(user_object) method will take user data sent from client and generate an encoded token to be sent back to client for user authentication and authorization purpose.
+
+    def self.create_token(user_object)
+        
+        payload  = {user: JSON.parse(user_object.to_json)}
+
+        token = JWT.encode(payload, "build", "HS256")
+    end
+
+    #self.decode_token(token) will receive a token from client and decode it and if token is successfully decoded, send back user details
+    
+    def self.decode_token(token)
+
+        decode = JWT.decode(token, "build", true, {algorithm: "HS256"})
+
+    end
+end
