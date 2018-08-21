e78df4a5d125b4efdeb033ed614eaed81b59a2d5
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-08-20 20:04:01 -0400

    Add models and migrations for User, Game and UserGame join table

---
+++ db/migrate/20180821000217_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :player1
+      t.string :player2
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
